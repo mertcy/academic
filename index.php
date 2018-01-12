08cfdf8f06650ac0af0fe31300ba3355607a1850
behlulkeremsisman <behlulkeremsisman@behlulkeremsismans-MacBook-Pro.local>
2018-01-12 20:31:27 +0300

    Index page button style is fixed.

--- index.php
+++ index.php
@@ -8,16 +8,14 @@
 </head>
 
 <body>
-
   <div class="wrapper">
-	<div class="container">
-		<h1>AcadeMic'e Hoş Geldiniz</h1>
-
-    <h2>Sisteme giriş yapmak için Giriş butonuna tıklayınız</h2>
-		<form action="/src/front-end/login.php">
-			<input type="submit" id="login-button" VALUE="Giriş"  onclick="window.location.href='/src/front-end/login.php'">
-		</form>
-	</div>
+    <h1>AcadeMic'e Hoş Geldiniz</h1>
+  	<div class="container">
+      <h2>Sisteme giriş yapmak için Giriş butonuna tıklayınız</h2>
+  		<form action="/src/front-end/login.php">
+  			<input type="submit" id="login-button" VALUE="Giriş"  onclick="window.location.href='/src/front-end/login.php'">
+  		</form>
+  	</div>
 
 	<ul class="bg-bubbles">
 		<li></li>
@@ -34,5 +32,6 @@
 
 </div>
 </body>
-
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  <script  src="js/index.js"></script>
 </html>
