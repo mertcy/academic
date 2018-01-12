f561c189b08dbf579422a619815aeeee49fa0d70
behlulkeremsisman <behlulkeremsisman@behlulkeremsismans-MacBook-Pro.local>
2018-01-12 18:26:20 +0300

    Index page created. Welcome section and login button that connects the login page is added. Login page is reorganized.

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>AcadeMic Anasayfa</title>
+      <link rel="stylesheet" href="src/css/style.css">
+</head>
+
+<body>
+
+  <div class="wrapper">
+	<div class="container">
+		<h1>AcadeMic'e Hoş Geldiniz</h1>
+
+    <h2>Sisteme giriş yapmak için Giriş butonuna tıklayınız</h2>
+		<form action="/src/front-end/login.php">
+			<input type="submit" id="login-button" VALUE="Giriş"  onclick="window.location.href='/src/front-end/login.php'">
+		</form>
+	</div>
+
+	<ul class="bg-bubbles">
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
+
+</div>
+</body>
+
+</html>
--- src/front-end/login.php
+++ src/front-end/login.php
@@ -1,67 +1,48 @@
 <?php
-        if(isset($_POST['submit'])) {
 
-					$username = $_POST['username'];
-					$password = $_POST['password'];
+  if(isset($_POST['submit']))
+  {
+			$username = $_POST['username'];
+			$password = $_POST['password'];
+      $connection = mysqli_connect('localhost','root','root','acadeMic','8889','/Applications/MAMP/tmp/mysql/mysql.sock');
 
-      $connection = mysqli_connect('localhost','root','root','academics','8889','/Applications/MAMP/tmp/mysql/mysql.sock');
-
-			if($connection) {
+			if($connection)
+      {
 				echo "databaseye bağlanıldı";
-			}else {
+			}
+      else
+      {
 				die("databaseye bağlanılamadı");
 			}
-
-
-				// if( $username && $password){
-				// 		echo $username;
-				// 	   echo $password;
-        //
-				// } else {
-				//   echo "geçersiz giriş";
-				// }
-
-		}
-
- ?>
+	 }
+?>
 
 
 <!DOCTYPE html>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Giriş</title>
+    <link rel="stylesheet" href="/src/css/login.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css">
+</head>
+
 <body>
-<!--<p style="text-align:center;"><img src="C:\MAMP\htdocs\academic\img\logo.jpg" width="400" height="341" alt="Öğrenci Girişi"></p>--> 
-  <head>
-  <style>
-  div.container {
-    position: relative;
-    top: 200px;
-    left: 100px;
-    width: 500px;
-  }
-  </style>
-  </head>
-  <div class="container">
-    <div class="col-sm-6">
-      <form action="login.php" method="post">
-        <div class="form-group">
-          <label for="username">Kullanıcı Adı</label>
-          <input type="text" name="username" class="form-control">
-        </div>
-
-        <div class="form-group">
-          <label for="password">Şifre</label>
-          <input type="password" name="password" class="form-control">
-        </div>
-
-				<input class="btn btn-primary" type="submit" name="submit" value="Giriş">
-</form>
+    <div class="container">
+      <div class="col-sm-6">
+        <form action="login.php" method="post">
+            <div class="form-group">
+              <label for="username">Kullanıcı Adı</label>
+              <input type="text" name="username" class="form-control">
+            </div>
+            <div class="form-group">
+              <label for="password">Şifre</label>
+              <input type="password" name="password" class="form-control">
+            </div>
+        	<input class="btn btn-primary" type="submit" name="submit" value="Giriş">
+        </form>
+      </div>
     </div>
-
-
-</div>
 </body>
 </html>
