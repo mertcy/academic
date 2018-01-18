f0442416c9199b3212179c2a067fa2fa8c4da2cb
oguzmertcaylar <oguzmertcaylar7@gmail.com>
2018-01-18 22:36:28 +0300

    #16: added logging in and login session for each type of user

--- src/front-end/login.php
+++ src/front-end/login.php
@@ -1,20 +1,68 @@
 <?php
+  session_start(); // start the session
 
-  if(isset($_POST['submit']))
-  {
+  if(isset($_POST['submit'])) {
 			$username = $_POST['username'];
 			$password = $_POST['password'];
-      $connection = mysqli_connect('localhost','root','root','acadeMic','8889','/Applications/MAMP/tmp/mysql/mysql.sock');
+      $connection = mysqli_connect('localhost','root','root','db_academic','8889','/Applications/MAMP/tmp/mysql/mysql.sock');
 
-			if($connection)
-      {
-				echo "databaseye bağlanıldı";
-			}
-      else
-      {
-				die("databaseye bağlanılamadı");
-			}
-	 }
+			if($connection) {
+          $sql_log = "SELECT kullanici_id, kullanici_tipi, kullanici_parolasi FROM Kullanici";
+          $result = mysqli_query($connection, $sql_log);
+          $valid = 0; // user not verified yet
+
+          while($row_log = mysqli_fetch_row($result)) {
+              $username_db = $row_log[0];
+              $userType = $row_log[1];
+              $password_db = $row_log[2];
+
+              if(($username == $username_db) && ($password == $password_db)) { // user credentials match
+                  $valid = 1; // user verified
+                  $_SESSION["user_id"] = $username;
+                  break;
+              }
+          }
+
+          mysqli_free_result($result);
+
+          if($valid == 1) { // user verified
+              $sql_info = "SELECT kisi_id, kisi_adi, kisi_soyadi FROM Kisi WHERE kisi_id = $username";
+              $result = mysqli_query($connection, $sql_info);
+
+              if($row_info = mysqli_fetch_row($result)) {
+                  $kisi_adi = $row_info[1];
+                  $kisi_soyadi = $row_info[2];
+                  mysqli_free_result($result);
+                  mysqli_close($conn);
+                  echo "<script type='text/javascript'>alert('$kisi_adi $kisi_soyadi olarak sisteme giriş yapılmıştır.');</script>";
+
+                  if($userType == 0) { // opens student's page
+                      header('Refresh: 1; URL = ogrenci.php');
+                      exit;
+                  } elseif ($userType == 1) { // opens parent's page
+                      header('Refresh: 1; URL = veli.php');
+                      exit;
+                  } elseif ($userType == 2) { // opens teacher's page
+                      header('Refresh: 1; URL = ogretmen.php');
+                      exit;
+                  }
+              }
+
+            } elseif($valid == 0) { // user not verified
+                mysqli_close($conn);
+                echo "<script type='text/javascript'>alert('Yanlış kullanıcı adı ve şifre kombinasyonu girilmiştir.');</script>";
+                unset($_POST['username']);
+                unset($_POST['password']);
+                session_unset(); // remove all session variables
+                session_destroy(); // destroy the session
+                header('Refresh: 1; URL = login.php');
+                exit;
+            }
+
+      }
+
+
+	}
 ?>
 
 
