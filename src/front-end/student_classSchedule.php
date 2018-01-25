d4596a78266bb2eca5b7678db14e8dc604debae5
oğulcan altınbek <ogulcanaltinbek@gmail.com>
2018-01-25 20:14:04 +0300

    #27: It was clear that classSchedule belongs which class

--- src/front-end/student_classSchedule.php
+++ src/front-end/student_classSchedule.php
@@ -1,6 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <?php
+    $connection = mysqli_connect('localhost','root','root','db_academic','8889','/Applications/MAMP/tmp/mysql/mysql.sock');
+
+    if($connection) {
+        $sql_log = "SELECT og.sinif_id, og.sube_id
+                    FROM Ogrenci AS og
+                    WHERE og.ogrenci_id = 1";
+        $result = mysqli_query($connection, $sql_log);
+
+        ?>
+        <p><?php
+          while($row_log = mysqli_fetch_row($result)) {
+            $sinif_id = $row_log[0];
+            $sube_id = $row_log[1];
+
+            ?>
+          <?php }} ?>
+      </p>
+   <h2><?php echo $sinif_id;?>-<?php echo $sube_id;?> Sınıfı Ders Programı</h2>
 <table style="width:100%" border="2">
   <tr>
     <th>Saat</th>
@@ -16,7 +35,7 @@
   </tr>
   <tr>
     <td>9:30-10:20</td>
-  >
+  
   </tr>
   <tr>
     <td>10:30-11:20</td>
