791b3736dde7cc23bb13567715c79f3eb2cfd1df
behlulkeremsisman <behlulkeremsisman@behlulkeremsismans-MacBook-Pro.local>
2018-02-21 22:59:33 +0300

    #48: Students can see the quiz in their 'Derslerim' page with a related course that the teacher sent.

--- src/front-end/student_class.php
+++ src/front-end/student_class.php
@@ -21,7 +21,28 @@
         mysqli_close($connection);
         ?>
         <h3><?php echo $ders_adi; ?></h3>
+        <br></br>
+        <h4>Dersin quizi:</h4>
         <?php
     }
 
+    $connection2 = mysqli_connect('localhost','root','root','db_academic','8889','/Applications/MAMP/tmp/mysql/mysql.sock');
+
+    if($connection2) {
+        $sql_log2 = "SELECT q.link
+                     FROM Quiz AS q, Ders AS d, Ogrenci AS o
+                     WHERE q.ders_id = d.ders_id AND o.ogrenci_id = $current_ogrenciId AND o.sinif_id = q.sinif_id AND o.sube_id = q.sube_id AND d.ders_adi = '$ders_adi'";
+        $result2 = mysqli_query($connection2, $sql_log2);
+
+        while($row_log2 = mysqli_fetch_row($result2)) {
+            $link = $row_log2[0];
+        }
+
+        mysqli_free_result($result2);
+        mysqli_close($connection2);
+        echo "$link";
+    }
 ?>
+        <script type="text/javascript" async defer src="https://d134jvmqfdbkyi.cloudfront.net/script/embed.min.js"></script>
+        <br></br>
+        ​<iframe width="950" height="450" src= <?php echo "$link"; ?> frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
