f789f4843d9464dbdd724fa5674a887abf7bf703
oğulcan altınbek <ogulcanaltinbek@gmail.com>
2018-02-23 16:30:18 +0300

    #30: Not Dokumum section is added at Student Page

--- src/front-end/student.php
+++ src/front-end/student.php
@@ -179,7 +179,25 @@
                         </ul>
                     </li>
                     <li>
-                        <a href="#">Not Dokumum</a>
+                      <a href="#NotDokumu" onclick="showGrades(<?php echo $current_ogrenciId; ?>)">Not Dökümüm</a>
+                      <script>
+                      function showGrades(ogrenci_url) {
+
+                        if (window.XMLHttpRequest) {
+                          // code for IE7+, Firefox, Chrome, Opera, Safari
+                          xmlhttp=new XMLHttpRequest();
+                        } else { // code for IE6, IE5
+                          xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+                        }
+                        xmlhttp.onreadystatechange=function() {
+                          if (this.readyState==4 && this.status==200) {
+                            document.getElementById("studentContent").innerHTML=this.responseText;
+                          }
+                        }
+                        xmlhttp.open("GET","student_grades.php?q="+ogrenci_url,true);
+                        xmlhttp.send();
+                      }
+                      </script>
                     </li>
                 </ul>
 
